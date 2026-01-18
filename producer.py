3a570ab1cb49c227da1f7f68d44d27d4997bee74
omer zilberberg <omer212111@gmail.com>
2026-01-18 17:15:51 +0200

    Refactor Kafka producer message handling

--- producer.py
+++ producer.py
@@ -2,13 +2,13 @@ import csv
 import time
 from kafka import KafkaProducer
 
-# הגדרות ה-Kafka
+
 BOOTSTRAP_SERVERS = 'localhost:9092'
 TOPIC_NAME = 'electric_vehicles'
 CSV_FILE_PATH = '/home/omer2/pyspark/input/Electric_Vehicle_Population_Data.csv'
 
 def run_producer():
-    # יצירת ה-Producer
+    
     producer = KafkaProducer(
         bootstrap_servers=BOOTSTRAP_SERVERS,
         value_serializer=lambda v: str(v).encode('utf-8')
@@ -22,16 +22,14 @@ def run_producer():
             header = next(reader)  # דילוג על שורת הכותרות
             
             for row in reader:
-                # הפיכת השורה למחרוזת אחת מופרדת בפסיקים (כמו ב-CSV המקורי)
+                
                 message = ",".join(row)
                 
-                # שליחת ההודעה ל-Kafka
+                
                 producer.send(TOPIC_NAME, value=message)
                 
-                # הדפסה קטנה לביקורת
                 print(f"Sent: {row[0]}, {row[1]}") 
                 
-                # השהייה קלה כדי לדמות זרם נתונים בזמן אמת
                 time.sleep(0.1) 
                 
     except Exception as e:
